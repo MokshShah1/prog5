228b7ae731821434b8e324c77de7573a7fa91883
Moksh <mps2965@eid.utexas.edu>
2026-02-20 17:09:55 -0600

    done

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ set -o pipefail
 
 cc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-asm.c -o hw5-asm
 cc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-sim.c -o hw5-sim
-cc -std=c11 -O2 -Wall -Wextra -Werror -pedantic test_hw5.c -o test_hw5
