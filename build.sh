381e4704123f48dfaf87b5e0f80c532df290036a
Moksh <mps2965@eid.utexas.edu>
2026-02-20 19:44:53 -0600

    done

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
+set -e
 
-CFLAGS="-std=c11 -O2 -Wall -Wextra -Werror -pedantic"
+gcc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-asm.c -o hw5-asm
 
-# Build assembler
-cc $CFLAGS main_asm.c pass1.c pass2.c helpers.c -o hw5-asm
-
-# Build simulator
-cc $CFLAGS main_sim.c machine.c execute.c -o hw5-sim -lm
-
-echo "Built: ./hw5-asm and ./hw5-sim"
+gcc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-sim.c -o hw5-sim -lm
