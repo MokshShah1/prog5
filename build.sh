b1e859c9cf5042e186cfd21c1e0cbdb6f1671904
Moksh <mps2965@eid.utexas.edu>
2026-02-21 22:18:55 -0600

    done

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 set -o pipefail
 
-cc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-asm.c -o hw5-assembler
-cc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-sim.c -o hw5-sim -lm
+gcc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-asm.c -o hw5-asm
+gcc -std=c11 -O2 -Wall -Wextra -Werror -pedantic hw5-sim.c -o hw5-sim -lm
